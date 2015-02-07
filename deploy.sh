f96999d3621751d81b852c73893630b81283c51e
Jake Pelletier <jake.pelletier@sendgrid.com>
2015-02-07 10:41:41 -0700

    Hacky backups

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-ln -s ./.vimrc ~/
-ln -s ./.tmux.conf ~/
-ln -s ./.gitconfig ~/
-ln -s ./.bashrc ~/
+if [ -e ~/.vimrc ]
+then
+    mv ~/.vimrc ~/.vimrc.bak
+fi
+
+if [ -e ~/.tmux.conf ]
+then
+    mv ~/.tmux.conf ~/.tmux.conf.bak
+fi
+
+if [ -e ~/.gitconfig ]
+then
+    mv ~/.gitconfig ~/.gitconfig.bak
+fi
+
+if [ -e ~/.bashrc ]
+then
+    mv ~/.bashrc ~/.bashrc.bak
+fi
+
+PWD=$(pwd)
+
+ln -s "$PWD/.vimrc" ~/
+ln -s "$PWD/.tmux.conf" ~/
+ln -s "$PWD/.gitconfig" ~/
+ln -s "$PWD/.bashrc" ~/
