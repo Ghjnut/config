549a72904ba5547427b7d734d0c9b0684b2826a9
Jake Pelletier <jake.pelletier@sendgrid.com>
2015-09-09 13:27:16 -0600

    Update deploy script for bash_profile

--- deploy.sh
+++ deploy.sh
@@ -20,9 +20,14 @@ then
     mv ~/.bashrc ~/.bashrc.bak
 fi
 
+if [ -e ~/.bash_profile ]
+then
+    mv ~/.bash_profile ~/.bash_profile.bak
+fi
 #PWD=$(pwd)
 
 ln -s "$(pwd)/.vimrc" "$HOME/"
 ln -s "$(pwd)/.tmux.conf" "$HOME/"
 ln -s "$(pwd)/.gitconfig" "$HOME/"
 ln -s "$(pwd)/.bashrc" "$HOME/"
+ln -s "$(pwd)/.bash_profile" "$HOME/"
