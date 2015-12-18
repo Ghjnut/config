04f74ce69f395450c58c9fd4eb36ffc636b83d26
Jake Pelletier <ghjnut@gmail.com>
2015-12-17 19:54:05 -0700

    Cleanup tpm install

--- scripts/common/tpm_install.sh
+++ scripts/common/tpm_install.sh
@@ -1,20 +1,7 @@
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+#!/usr/bin/env bash
+[[ "$TRACE" ]] && set -x
+set -eu -o pipefail
 
-#cat << EOF
-## List of plugins
-## Supports github_username/repo or full git repo URLs
-#set -g @tpm_plugins ' \\
-#tmux-plugins/tpm \\
-#tmux-plugins/tmux-sensible \\
-#'
-## Other examples:
-## github_username/plugin_name
-## git@github.com/user/plugin
-## git@bitbucket.com/user/plugin
-#
-## Initializes TMUX plugin manager.
-## Keep this line at the very bottom of tmux.conf.
-#run-shell '~/.tmux/plugins/tpm/tpm'
-#EOF
+git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
 
 tmux source-file ~/.tmux.conf
